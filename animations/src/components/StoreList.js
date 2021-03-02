a9f307e13547a9eb868effebe4e679abc4a5ac57
kelvinrandu <randukelvin@gmail.com>
2021-03-02 11:11:21 +0300

    added stores constant

--- animations/src/components/StoreList.js
+++ animations/src/components/StoreList.js
@@ -1,8 +1,16 @@
+import { getDomainLocale } from 'next/dist/next-server/lib/router/router';
 import Link from 'next/link';
 
 
-export default function Stores({stores}) {
 
+export default function Stores() {
+  const stores =[
+    {name: 'cyberplus', email:'cyberplusdiani@gmail.com' , location: 'diani', created: 'diani'  },
+    {name: 'wheelbarrow', quantity: 25 , buying_price: 5000 , selling_price: 'diani'  },
+    {name: 'gumboots', quantity: 50 , buying_price: 5000 , selling_price: 'diani'  },
+    {name: 'bamburi cement', quantity: 15 , buying_price: 5000 , selling_price: 'diani'  }
+  
+  ]
     
     return (
       <div>
