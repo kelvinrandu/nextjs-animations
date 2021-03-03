f4401b71ce0f195ee240d61d324cb3591c5c25ec
dave <devmuangi@gmail.com>
2021-03-03 10:10:15 +0300

    added chakraui

--- animations/src/pages/_app.js
+++ animations/src/pages/_app.js
@@ -1,5 +1,20 @@
 import '../../styles/index.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+    background:"black",
+  },
+}
+
+
+const theme = extendTheme({ colors })
+
+// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}><Component {...pageProps} /></ChakraProvider>
 }
