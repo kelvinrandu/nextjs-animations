69fbaaccfbf68fe2b232181fce52e9a8705d4da8
dave <devmuangi@gmail.com>
2021-03-03 08:29:51 +0300

    added _app.js

---
+++ animations/src/pages/_app.js
@@ -0,0 +1,6 @@
+import '../../styles/index.css';
+import App from "next/app";
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
