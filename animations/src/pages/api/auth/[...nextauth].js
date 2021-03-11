702132acec2d149ffb0ff320a7311f15d966e750
kelvinrandu <randukelvin@gmail.com>
2021-03-11 08:24:14 +0300

    added github signin

--- animations/src/pages/api/auth/[...nextauth].js
+++ animations/src/pages/api/auth/[...nextauth].js
@@ -7,19 +7,11 @@ const options = {
 
   // Configure one or more authentication providers
   providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
 
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    Providers.LinkedIn({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
 
   ],
 
