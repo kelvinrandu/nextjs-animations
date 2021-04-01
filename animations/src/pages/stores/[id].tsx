337e3bbbdb20145e5cbb5020cd41b735b5a5f8ba
kelvinrandu <randukelvin@gmail.com>
2021-04-01 10:51:11 +0300

    added get each store function in each store page

--- animations/src/pages/stores/[id].tsx
+++ animations/src/pages/stores/[id].tsx
@@ -1,8 +1,33 @@
 import { useRouter } from 'next/router';
 
+
+export async function getStaticPaths() {
+  const res = await fetch('http://localhost:3000/api/stores/')
+  const stores = await res.json()
+
+  const paths = stores.map((store) => ({
+    params: { id: store.id.toString() },
+  }))
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({params}) {
+  const res = await fetch(`http://localhost:3000/api/store/${params.id}`)
+  const store = await res.json()
+  console.log(store)
+  
+  return {
+    props : { store}
+  }
+  
+}
+
+
 export default function Store() {
     const router = useRouter();
-    console.log(router.query.id);
+    
+
 
  
     
